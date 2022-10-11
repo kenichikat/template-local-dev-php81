c00e8306f7b497adc81c0d2bc3ce3d670004dee5
Kenichi Kato <k3nichikato@gmail.com>
2022-10-11 19:42:55 +0900

    日本語化まで

--- php-dev/www/mynews/vite.config.js
+++ php-dev/www/mynews/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from 'path'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/js/app.js', 'resources/sass/app.scss',
+                'resources/sass/app.scss',
                 'resources/sass/admin.scss',
-                'resources/sass/profile.scss'
+                'resources/sass/profile.scss',
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
-    ]
+    ],
 });
